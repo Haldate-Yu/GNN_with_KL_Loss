8b70f8884151bb428a53f3146c2e83981091b416
Haldate <603539099@qq.com>
2023-02-27 15:48:51 +0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -24,3 +24,33 @@ def logger(model_name, loss, acc, duration, args):
             f.write('{}\n {}\n {}\n {}\n'.format(StartLine, BaseParams, KLparams, Result))
         else:
             f.write('{}\n {}\n {}\n'.format(StartLine, BaseParams, Result))
+
+def csv_logger(model_name, loss, acc, duration, args):
+    os.makedirs('./results', exist_ok=True)
+    os.makedirs('./results/{}'.format(args.dataset), exist_ok=True)
+    file = './results/{}/{}.csv'.format(args.dataset, model_name)
+
+    headerList = [
+        'Method', 'Dataset', 
+        # Base Params
+        'Learning Rate', 'Weight Decay', 'Epochs', 'Hidden Dims', 
+        # KL Params
+        'MLP Dims', 'alpha1', 'alpha2', 'Attn TopK', 
+        # Results
+        'Val Loss', 'Test Acc', 'Duration'
+    ]
+
+    with open(file, 'a+') as f:
+        # csv reader verification
+        f.seek(0)
+        header = f.read(6)
+        if header != "Method":
+            dw = csv.DictWriter(f, delimiter=',', fieldnames=headerList)
+            dw.writeheader()
+        content = "{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {:.4f}, {:.4f} ± {:.3f}, {}\n".format(
+            model_name, args.dataset, 
+            args.lr, args.weight_decay, args.epochs, args,hidden, 
+            args.hidden_mlp, args.kl_alpha1, args.kl_alpha2, args.attn_topk, 
+            float(loss.mean()), float(acc.mean()), float(acc.std()), float(duration.mean())
+        )
+        f.write(content)
